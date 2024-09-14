081f215d77a8dbe13bf5c70e38fc319edb5e11fe
Scotchex <barkodadam@gmail.com>
2024-09-14 17:17:58 -0400

    balls

---
+++ flask-server/gpt_i.py
@@ -0,0 +1,26 @@
+from openai import OpenAI
+from sttxt import listen
+
+API_KEY = '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'  # Replace with your actual API key
+MODEL="gpt-4o-mini"
+
+
+client = OpenAI(
+    api_key = API_KEY,
+)
+def ask_gpt(content):
+    completion = client.chat.completions.create(
+    model=MODEL,
+        messages=[
+            {
+                "role": "system",
+            
+                "content":  """You are going to assist a program called CrossDash. You will be giving first aid 
+                advice according to their input as bullet points moreover, DO NOT TELL THEM TO CALL EMERGENCY SERVICES OR HELP, as we will be calling it for them. 
+                Also don't forget these people will be panicking, therefore give short and useful answers which they can apply immediately. 
+                Here is their input: """ +
+                content,
+            }
+        ])
+    response = completion.choices[0].message.content
+    return response
--- flask-server/speech_synthesis.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-engine = pyttsx3.init()
-
-voices = engine.getProperty('voices')
-
-engine.setProperty('voices', 'com.apple.eloquence.en-US.Rocko')
-engine.say('Hello!')
-engine.runAndWait()
-
